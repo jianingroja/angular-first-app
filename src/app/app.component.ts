a40c68004735058c82d5aff1b65298e836f8f3b6
wangjianing <jianingmica@gmail.com>
2023-10-17 12:54:36 +0200

    feat: setup first app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Hello world from {{ name }}!</h1>`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'first-app';
+  name = 'jianing';
 }
